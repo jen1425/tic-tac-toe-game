265a5c61e0631ec16c6d3e01e57fe59c3017980b
jen1425 <jenniferhwa@gmail.com>
2017-07-17 15:07:10 -0700

    Add getMove() to get player move

--- game.js
+++ game.js
@@ -1,19 +1,27 @@
+var readlineSync = require('readline-sync');
+
 // Vars
-  var player = 'x';
-  var moves = 0;
-  var board = [[' ',' ',' '], [' ', ' ', ' '], [' ',' ',' ']];
-  var move = []; // player's move
+var player = 'x';
+var moves = 0;
+var board = [[' ',' ',' '], [' ', ' ', ' '], [' ',' ',' ']];
+var move = []; // player's move
 
-
+// getMove: gets the players move from terminal
+var getMove = function() {
+  // get inputed value in form row, column
+  let userMove = readlineSync.question('Your move (r,c)? ');
+  let row = userMove[0]; 
+  let col = userMove[2];
+  return [row, col];
+};
 
 // Outer while loop that alternates between x and o move
 while (moves < 8) {
 // Read player 1 move (x)
+move = getMove();
 // Update Board 
 // Display Board
 // Check if winner
-  
-
 // Toggles player turn x or o
 
 }
