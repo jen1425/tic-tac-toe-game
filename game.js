6dc2cbc3c200773969e5b743cb0785d9226ea55c
jen1425 <jenniferhwa@gmail.com>
2017-07-17 14:47:44 -0700

    Set up game fmwk

---
+++ game.js
@@ -0,0 +1,22 @@
+// Vars
+  var player = 'x';
+  var moves = 0;
+  var board = [[' ',' ',' '], [' ', ' ', ' '], [' ',' ',' ']];
+  var move = []; // player's move
+
+
+
+// Outer while loop that alternates between x and o move
+while (moves < 8) {
+// Read player 1 move (x)
+// Update Board 
+// Display Board
+// Check if winner
+  
+
+// Toggles player turn x or o
+
+}
+
+
+// Repeat until Win or Tie
